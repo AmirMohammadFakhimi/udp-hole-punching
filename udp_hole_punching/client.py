d74f6b5c755f9e36353cbbf0c3a1c7929b037348
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2024-01-28 16:05:26 +0330

    Add comment

--- udp_hole_punching/client.py
+++ udp_hole_punching/client.py
@@ -1,10 +1,11 @@
 from socket import *
 import datetime
 
-
+# an arbitrary message
 message = input('Enter message: ')
 message = f'{datetime.datetime.now()}: {message}'
 
+# getting a port and id from the user and also the destination id
 host = ''
 port = int(input('Enter port: '))
 my_id = input('Enter your id: ')
@@ -14,13 +15,16 @@ server_socket = socket(AF_INET, SOCK_DGRAM)
 server_socket.bind((host, port))
 print(f'client running on port {port}...')
 
-stun_server_address = ('localhost', 12345)
+# sending a message to the server in the format of 'my_id destination_id'
+stun_server_address = ('141.11.45.90', 12345)
 server_socket.sendto(f'{my_id} {destination_id}'.encode(), stun_server_address)
 
+# receiving the public address (IP) of the client from the server
 my_public_address, _ = server_socket.recvfrom(1024)
 my_public_address = my_public_address.decode()
 print(f'My public address: {my_public_address}')
 
+# receiving the public address (IP and port) of the destination client from the server
 destination_public_address, _ = server_socket.recvfrom(1024)
 destination_public_address = destination_public_address.decode()
 destination_public_address = destination_public_address.split(':')
@@ -30,6 +34,8 @@ print(f'Destination public address: {destination_public_address}')
 print(f'{my_public_address}:{port}:{my_id} -> '
       f'{destination_public_address[0]}:{destination_public_address[1]}:{destination_id}')
 
+# sending the message to the destination client
+# After that the connection will be open and the two clients can communicate with each other
 while True:
     try:
         server_socket.sendto(message.encode(), destination_public_address)
--- udp_hole_punching/stun_server.py
+++ udp_hole_punching/stun_server.py
@@ -5,6 +5,7 @@ def convert_address_to_string(address):
     return f'{address[0]}:{address[1]}'.encode()
 
 
+# running server on port 12345
 host = ''
 port = 12345
 server_socket = socket(AF_INET, SOCK_DGRAM)
@@ -16,26 +17,41 @@ id_address = {}
 # source_id: destination_id
 pending_requests = {}
 
+# running server
 while True:
     try:
+        # receiving a message from client
         message, address = server_socket.recvfrom(1024)
         decoded_message = message.decode()
         print(f'Received message from {address}: {decoded_message}')
 
+        # each client just sends one message to the server
+        # the message is in the format of 'source_id destination_id'
+        # 'source_id' is the id of the client that sent the message
+        # 'destination_id' is the id of the client that the source client wants to communicate with
         try:
             source_id, destination_id = decoded_message.split(' ', 1)
         except ValueError:
             print('Invalid message format')
             continue
+
+        # saving the address of the client that sent the message
         id_address[source_id] = address
+        # sending the public address of the client that sent the message to the server
         server_socket.sendto(address[0].encode(), address)
 
         if destination_id in pending_requests and pending_requests[destination_id] == source_id:
+            # if the destination client already sent a message to the server
+            # and the source client is the one that the destination client wants to communicate with
+            # then send the public addresses of the two clients to each other
             print(id_address[source_id], id_address[destination_id])
             server_socket.sendto(convert_address_to_string(id_address[source_id]), id_address[destination_id])
             server_socket.sendto(convert_address_to_string(id_address[destination_id]), id_address[source_id])
             del pending_requests[destination_id]
         else:
+            # if the destination client didn't send a message to the server
+            # or the source client is not the one that the destination client wants to communicate with
+            # then save the request in the pending requests dictionary
             pending_requests[source_id] = destination_id
 
     except KeyboardInterrupt:
